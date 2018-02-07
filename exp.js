4bf4e630aba4b15fe810b3b4db888badb22fa3de
Isaac <menardisaac@gmail.com>
2018-02-07 13:54:12 +0100

    mise en place fichier public

--- exp.js
+++ exp.js
@@ -1,3 +1,14 @@
 
-	var texte = parseInt(document.text.text.nom)	
-	alert(texte)								
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+	res.sendfile('./index.html')
+})
+app.use(express.static('public'));
+
+app.listen(2999, function () {
+  console.log('Example app listening on port 3000!')
+})	
+		
+				
