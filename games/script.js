92f7d2da1047609131defeeb154d63db0ebc223d
Isaac <35576419+isaacmenard@users.noreply.github.com>
2019-03-22 12:31:30 +0100

    portfolio

---
+++ games/script.js
@@ -0,0 +1,11 @@
+function action(x) {
+    if (x == "") {
+        return "veuillez entrer un chiffre SVP"
+    }
+    else {
+        result = []
+        for (var i = 0; i < x.length; i++) {
+            result = result.push("test")
+        }
+    }
+}
