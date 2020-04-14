56123c468787c3c24e59a2d3ab57878e0cd25c18
isaacmenard <menardisaac@gmail.com>
2020-04-14 19:44:27 +0200

    add page

--- foxSkied/script.js
+++ foxSkied/script.js
@@ -3,3 +3,18 @@ $(document).ready(function() {
 	newStyle.innerHTML = ".background{height:100%;width:auto;visibility:visible}";
 	document.body.appendChild(newStyle)
 });
+var hover = false
+function load(){
+	if(hover == false){
+		hover = true
+		document.getElementById("home").style.top = "-100%"
+		document.getElementById("textHome").style.top = "0%"
+		document.getElementById("montageNext").className = "arrow top" 
+	}else{
+		hover = false
+		document.getElementById("home").style.top = "0%"
+		document.getElementById("textHome").style.top = "100%"
+		document.getElementById("montageNext").className = "arrow" 
+	}
+	
+}
