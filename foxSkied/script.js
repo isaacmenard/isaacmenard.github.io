bafc86df76a61a1ea8cb28cbc5561ffa0b85162d
isaacmenard <menardisaac@gmail.com>
2020-04-14 18:01:28 +0200

    skiedFox

---
+++ foxSkied/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    var newStyle = document.createElement("style");
+	newStyle.innerHTML = ".background{height:100%;width:auto;visibility:visible}";
+	document.body.appendChild(newStyle)
+});
---
+++ newPortFolio/script.js
@@ -0,0 +1,10 @@
+function load(page){
+	var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				document.body.innerHTML = (this.responseText)
+			}
+		};
+		xmlhttp.open("GET", page+".html", true);
+		xmlhttp.send();
+}
