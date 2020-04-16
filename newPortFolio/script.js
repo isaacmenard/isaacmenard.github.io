c3314a1bd904a6bd11bee71fa58fdb4785c867c5
isaacmenard <menardisaac@gmail.com>
2020-04-16 11:30:33 +0200

    abut

--- newPortFolio/script.js
+++ newPortFolio/script.js
@@ -1,10 +1,4 @@
-function load(page){
-	var xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange = function () {
-			if (this.readyState == 4 && this.status == 200) {
-				document.body.innerHTML = (this.responseText)
-			}
-		};
-		xmlhttp.open("GET", page+".html", true);
-		xmlhttp.send();
+page = 0
+function changePage(number){
+	
 }
