668cc06cead02615b767f4cbbde913c4970840ba
isaacmenard <menardisaac@gmail.com>
2020-04-21 20:21:31 +0200

    monatge

--- newPortFolio/script.js
+++ newPortFolio/script.js
@@ -1,4 +1,18 @@
-page = 0
-function changePage(number){
-	
+launchTitle()
+function launchTitle(){
+	document.getElementById("title").className=("glitch glitchAnim")
+	setTimeout(function(){
+		document.getElementById("title").className=("glitch")
+		setTimeout(function(){
+			launchTitle()
+		},Math.random() * (5000 - 3000) + 3000)
+	},Math.random() * (2000 - 200) + 200)
+}
+
+function changePage(){
+	document.getElementById("title").className=("glitch glitchAnim")
+	document.getElementById("title").style.fontSize = "1000%"
+	setTimeout(function(){
+		document.getElementById("title").style.visibility = "hidden"
+	},200)
 }
