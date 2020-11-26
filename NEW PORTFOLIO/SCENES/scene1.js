cb751e7846e122b6027782598bf5a324def33cf0
isaacmenard <menardisaac@gmail.com>
2020-11-26 18:56:45 +0100

    add tileSet

--- NEW PORTFOLIO/SCENES/scene1.js
+++ NEW PORTFOLIO/SCENES/scene1.js
@@ -15,95 +15,59 @@ class selectWorld extends Phaser.Scene{
 			this.load.spritesheet('portal', 'ASSETS/FIGHTGAME_Assets/ITEMs/bonus4.png', { frameWidth: 25, frameHeight: 25});
 			this.load.image('chest', 'ASSETS/FIGHTGAME_Assets/ITEMs/chest.png');
 			this.load.image('texte1', 'ASSETS/TEXTE/texte1.png');
+			this.load.image('textWelcome', 'ASSETS/TEXTE/welcome.png');
+			this.load.image('textPortfolio', 'ASSETS/TEXTE/portfolio.png');
+			this.load.image('textProjet', 'ASSETS/TEXTE/projetsAnnonce.png');
+			this.load.image('textIam', 'ASSETS/TEXTE/textIam.png');
 			this.load.image('checkpointText', 'ASSETS/TEXTE/checkpoint.png');
 			this.load.spritesheet('dude', 'ASSETS/FIGHTGAME_Assets/CHARAs/NAMKA/running/running.png', { frameWidth: 19, frameHeight: 41 });
 			this.load.spritesheet('dudeAttack', 'ASSETS/FIGHTGAME_Assets/CHARAs/NAMKA/attack/attack.png', { frameWidth: 71, frameHeight: 41 });
 			this.load.spritesheet('boom', 'ASSETS/FIGHTGAME_Assets/FXs/BOUM/BOOM_1.png', { frameWidth: 303, frameHeight: 79 });
+			this.load.image('tiles', 'ASSETS/adve/tiles.png');
+  			// Load the export Tiled JSON
+  			this.load.tilemapTiledJSON('map', 'ASSETS/Map/mapStart.json');
 		}
 		create () {
 			listCheckPoint = [];
 			checkPoint = [100,450+vertical,null]
 			//ajout images Map
+			
+			
 			background = this.add.image(2000, window.innerHeight - (300*window.innerHeight/600), 'background').setScale(window.innerHeight/600);
-			platforms = this.physics.add.staticGroup();
-			tree = this.physics.add.staticGroup();
-			props = this.physics.add.staticGroup();
-			tree.create(50, 540+vertical, 'tree').setScale(5.5).refreshBody();
-			tree.create(711, 640+vertical, 'tree').setScale(3.5).refreshBody();
-			tree.create(2011, 745+vertical, 'tree').setScale(4.5).refreshBody();
-			tree.create(1211, 790+vertical, 'tree').setScale(3.5).refreshBody();
-			tree.create(1411, 790+vertical, 'tree').setScale(3.5).refreshBody();
-		    tree.create(2711, 845+vertical, 'tree').setScale(4.5).refreshBody();
-			platforms.create(50, 200+vertical, 'ground').setScale(2.5).refreshBody();
-			platforms.create(200, 568+vertical, 'ground').setScale(2.5).refreshBody();
-			platforms.create(700, 418+vertical, 'ground').setScale(2.5).refreshBody();
-			platforms.create(1300, 568+vertical, 'ground').setScale(2.5).refreshBody();
-			platforms.create(2000, 468+vertical, 'ground').setScale(2.5).refreshBody();
-			platforms.create(2700, 568+vertical, 'ground').setScale(2.5).refreshBody();
-			props.create(50, 552+vertical, 'prop1').setScale(1.5).refreshBody();
-			props.create(2750, 552+vertical, 'prop1').setScale(1.5).refreshBody();
-			props.create(2650, 552+vertical, 'prop1').setScale(1.5).refreshBody();
-			props.create(1150, 554+vertical, 'prop1').setScale(1.2).refreshBody();
-			props.create(1350, 552+vertical, 'prop1').setScale(1.5).refreshBody();
-			props.create(780, 438+vertical, 'prop2').setScale(3).refreshBody();
-			props.create(2100, 488+vertical, 'prop2').setScale(3).refreshBody();
-			props.create(80, 588+vertical, 'prop2').setScale(3).refreshBody();
-			props.create(2780, 588+vertical, 'prop2').setScale(3).refreshBody();
-			props.create(180, 218+vertical, 'prop3').setScale(3).refreshBody();
-			props.create(1380, 583+vertical, 'prop3').setScale(3).refreshBody();
-			props.create(260, 552+vertical, 'prop4').setScale(3).refreshBody();
-			props.create(20, 179+vertical, 'chest').setScale(2).refreshBody();
-			props.create(80, 185+vertical, 'prop1').setScale(1.7).refreshBody();
-			props.create(680, 402+vertical, 'prop1').setScale(1.4).refreshBody();
-			props.create(2000, 452+vertical, 'prop1').setScale(1.4).refreshBody();
-			props.create(2080, 454+vertical, 'prop1').setScale(1.7).refreshBody();
-			props.create(1900, 452+vertical, 'prop4').setScale(3).refreshBody();
+			map = this.make.tilemap({ key: 'map' });
+			const tileset = map.addTilesetImage('test', 'tiles');
+			backgroundTiled = map.createStaticLayer('BACKGROUND', tileset, 0, 0).setScale(scaleMap);
+			platformsTiled = map.createStaticLayer('COLLIDE', tileset, 0, 0).setScale(scaleMap);
+			deadTouchTiled = map.createStaticLayer('DEADTOUCH', tileset, 0, 0).setScale(scaleMap);
+			elementsTiled = map.createStaticLayer('ELEMENTS', tileset, 0, 0).setScale(scaleMap);
+			platformsTiled.setCollisionByExclusion(-1, true);			
+	
+			const spawnPoint = map.getObjectLayer('SPAWNPOINT')['objects'][0];
+			checkPoint = [spawnPoint.x*scaleMap,spawnPoint.y*scaleMap,null]
 			
-			
-			text0 = this.physics.add.sprite(150, -100, 'texte1').setScale(0.4);
-			text0.body.allowGravity = false;
-			//textList.push(text0);
-			text1 = this.physics.add.sprite(750, -100, 'texte1').setScale(0.4);
-			text1.body.allowGravity = false;
-			//textList.push(text1);
-
-			textTuto = this.physics.add.sprite(755, 350+vertical, 'checkpointText').setScale(0.2);
-			textTuto.body.allowGravity = false;
-
-			
-			checkPoint0 = this.physics.add.sprite(100, 510+vertical, 'checkPoint').setScale(2.5);
-			checkPoint0.body.allowGravity = false
-			listCheckPoint.push(checkPoint0)
-			checkPoint1 = this.physics.add.sprite(710, 360+vertical, 'checkPoint').setScale(2.5);
-			checkPoint1.body.allowGravity = false
-			listCheckPoint.push(checkPoint1)
-			checkPoint2 = this.physics.add.sprite(1310, 510+vertical, 'checkPoint').setScale(2.5);
-			checkPoint2.body.allowGravity = false
-			listCheckPoint.push(checkPoint2)
-			portal = this.physics.add.sprite(2708, 510+vertical, 'portal').setScale(2.5);
-			portal.body.allowGravity = false
-			if(!window.mobileCheck){
-				boom = this.add.sprite(-10000, (window.innerHeight - (303*1.5/2)) , 'boom').setScale(1.5);
+			if(window.mobileCheck() == false){
+				boom = this.add.sprite(-10000, (map.heightInPixels*scaleMap - (303*1.5/2)) , 'boom').setScale(1.5);
 			}else{
-				boom = this.add.sprite(-10000, (window.innerHeight - (303*1.5/2) - window.innerHeight*0.20) , 'boom').setScale(1.5);
+				boom = this.add.sprite(-10000, (map.heightInPixels*scaleMap- (303*1.5/2) - map.heightInPixels*scaleMap*0.20) , 'boom').setScale(1.5);
 			}
 			boom.rotation = 4.7
 
 			player = this.physics.add.sprite(checkPoint[0],checkPoint[1], 'dude').setScale(3);
-			this.physics.add.collider(player, platforms);
+			this.physics.add.collider(player, platformsTiled);
 
 			cursors = this.input.keyboard.createCursorKeys()
 			var cam = this.cameras.main
-			cam.setBounds(0, 0, 3800, 600);
+			cam.setBounds(0, 0, map.widthInPixels*scaleMap, map.heightInPixels*scaleMap);
 			cam.startFollow(player,cam.FOLLOW_LOCKON, 0.1, 0.1);
 			var firstZoom = true
+
 			function zoomCamera(){
 				if(firstZoom){
 					cam
 						.setPosition(cam.x,-vertical - 800)
 						.setSize(window.innerWidth, window.innerHeight*3)
 						.setZoom(2);
-					background.y += window.innerHeight/2
+					background.y +=  window.innerHeight/2
 					firstZoom = false
 				}else{
 					cam
@@ -118,43 +82,55 @@ class selectWorld extends Phaser.Scene{
 			}
 			background.setScrollFactor(0.5);
 
-			//anim CHeckPoint
-			function checkPointF(checkPointSelectId){
-					checkPoint=[listCheckPoint[checkPointSelectId].x,listCheckPoint[checkPointSelectId].y-100,listCheckPoint[checkPointSelectId]]
+					//anim CHeckPoint
+			function checkPointF(checkPointSelect){
+					checkPoint=[checkPointSelect.x,checkPointSelect.y-200,checkPointSelect]
 					if(startAnim == false){
 						startAnim = true;
 						var interval = setInterval(function (){
-							if(Math.round(listCheckPoint[checkPointSelectId].angle +180) < 360 && Math.round(listCheckPoint[checkPointSelectId].angle +180) > 0){
-								listCheckPoint[checkPointSelectId].angle += 3
+							if(Math.round(checkPointSelect.angle +180) < 360 && Math.round(checkPointSelect.angle +180) > 0){
+								checkPointSelect.angle += 3
 							}else{
 								startAnim = false
 								clearInterval(interval)
 							}
 						},1)
-						if(checkPointSelectId == 1){
-							var intervalN = setInterval(function (){
-								if(textTuto.alpha > 0){
-								   textTuto.alpha -= 0.1;
-								}else{
-									clearInterval(intervalN)
-								}
-							},100)
-						}
 					}
-				if(textList[checkPointSelectId] != undefined){
-						if(textList[checkPointSelectId].y < 150){
-							textList[checkPointSelectId].setVelocityY(120)
-						}else{
-							textList[checkPointSelectId].setVelocityY(0)
-						}
-					}
+
 				}
 
-			//listCheckPoint
-			this.physics.add.overlap(player, checkPoint0, function(){checkPointF(0)});
-			this.physics.add.overlap(player, checkPoint1, function(){checkPointF(1)});
-			this.physics.add.overlap(player, checkPoint2, function(){checkPointF(2)});
+			 	// Create a sprite group for all spikes, set common properties to ensure that
+			// sprites in the group don't move via gravity or by player collisions
+			checkPoints = this.physics.add.group({
+			  allowGravity: false,
+			  immovable: true
+			});
 			
+			// Let's get the spike objects, these are NOT sprites
+			const checkPointObjects = map.getObjectLayer('CHECKPOINT')['objects'];
+
+			// Now we create spikes in our sprite group for each object in our map
+			checkPointObjects.forEach(checkPointS => {
+			  // Add new spikes to our sprite group, change the start y position to meet the platform
+			  var myCheckPoint = this.physics.add.sprite(checkPointS.x*scaleMap, checkPointS.y*scaleMap  , 'checkPoint').setScale(2.5);
+			
+			  myCheckPoint.body.allowGravity = false;
+			  myCheckPoint['idS'] = listCheckPoint.length
+			  listCheckPoint.push(myCheckPoint)
+				
+				this.physics.add.overlap(player, myCheckPoint, function(){checkPointF(myCheckPoint)});
+			});
+			var portalObjects = []
+			const portalPointObjects = map.getObjectLayer('PORTAL')['objects'];
+
+			// Now we create spikes in our sprite group for each object in our map
+			portalPointObjects.forEach(checkPointS => {
+			  // Add new spikes to our sprite group, change the start y position to meet the platform
+			  var myPortalPoint = this.physics.add.sprite(checkPointS.x*scaleMap, checkPointS.y*scaleMap  , 'portal').setScale(2.5);
+			myPortalPoint['sceneGo'] = checkPointS.properties[0].value
+			  myPortalPoint.body.allowGravity = false;
+				portalObjects.push(myPortalPoint)
+			});
 			
 			//list animation
 			this.anims.create({
@@ -220,19 +196,26 @@ class selectWorld extends Phaser.Scene{
 			player.anims.play('turnRight');
 			
 			
+			
 			//coup épée space
 			sword = function (){
 				
 				player.setVelocityX(0);
+				
+				
+
+			// Now we create spikes in our sprite group for each object in our map
+			portalObjects.forEach(checkPointS => {
+				
 				//action si il est à coté du portal sinon juste attaque
 				if(lastPos == "left"){
-					if(portal.x - player.x > -100 && portal.x - player.x < 0){
+					if(checkPointS.x - player.x > -100 && checkPointS.x - player.x < 0){
 						zoomCamera()
 						setTimeout(function(){
 							player.anims.play('attackLeft', true);
-							portal.anims.play('portalAnim')
+							checkPointS.anims.play('portalAnim')
 							setTimeout(function(){
-								game.scene.start("scene2");
+								game.scene.start(checkPointS.sceneGo);
 								game.scene.stop("selectWorld");
 							},1000	)
 						},400)
@@ -241,13 +224,13 @@ class selectWorld extends Phaser.Scene{
 					}
 				}
 				else{
-					if(portal.x - player.x < 100){
+					if(checkPointS.x - player.x < 100){
 						zoomCamera()
 						setTimeout(function(){
-							portal.anims.play('portalAnim')
+							checkPointS.anims.play('portalAnim')
 							player.anims.play('attackRight', true)
 							setTimeout(function(){
-								game.scene.start("scene2");
+								game.scene.start(checkPointS.sceneGo);
 								game.scene.stop("selectWorld");
 							},1000)
 						},400)
@@ -255,13 +238,16 @@ class selectWorld extends Phaser.Scene{
 						player.anims.play('attackRight', true)
 					}
 				}
+			});
 			}
+									   
 			
 			this.input.keyboard.on('keyup_SPACE', function (event) {
 				sword()
 			})
 		}
 		update () {
+			
 			//spaceUp pour mobile
 			if(keyDown[3] == true){
 				spaceDown = true
@@ -271,30 +257,22 @@ class selectWorld extends Phaser.Scene{
 				sword()
 			}
 			
+			
 			//animation checkpoint
 			for(var i = 0; i < listCheckPoint.length;i++){
 				if(listCheckPoint[i].angle != 0 && checkPoint[2] != listCheckPoint[i]){
 					listCheckPoint[i].angle -= 6
 				}
 			}
-			//animation text
-			for(var i = 0; i < textList.length;i++){
-				if(startAnim == false){
-					if(textList[i].y > -100){
-						textList[i].setVelocityY(-120)
-					}else{
-						textList[i].setVelocityY(0)
-					}
-				}
-			}
-			
+
 			//retourCheckPoint
-			if(player.y >  (window.innerHeight +100)){
+			if(player.y >  (map.heightInPixels*scaleMap +100)){
 				boom.x = player.x
 				player.x = checkPoint[0]
 				player.y = checkPoint[1]
 				boom.anims.play('boom', true);
 			}
+		
 			
 			//animation arret
 			if(cursors.left.isUp && cursors.right.isUp && keyDown[0] == false && keyDown[1] == false && player.body.velocity.x != 0){
@@ -306,32 +284,36 @@ class selectWorld extends Phaser.Scene{
 					player.anims.play('turnRight')
 				}
 			}
-			//deplacement + saut
-			if (cursors.left.isDown || keyDown[1] == true) {
-				player.setVelocityX(speedPerso * -1);
-				if (player.body.touching.down) { 
-					player.anims.play('left', true);
-				}else{
-					player.anims.play('leftJump', true);
+			
+		
+			if(canMove == true){
+				//deplacement + saut
+				if (cursors.left.isDown || keyDown[1] == true) {
+					player.setVelocityX(speedPerso * -1);
+					if (player.body.onFloor()) { 
+						player.anims.play('left', true);
+					}else{
+						player.anims.play('leftJump', true);
+					}
+					lastPos = "left"
 				}
-				lastPos = "left"
-			}
-			else if (cursors.right.isDown || keyDown[0] == true) {
-				player.setVelocityX(speedPerso);
-				if (player.body.touching.down) { 
-					player.anims.play('right', true);
-				}else{
-					player.anims.play('rightJump', true);
+				else if (cursors.right.isDown || keyDown[0] == true) {
+					player.setVelocityX(speedPerso);
+					if (player.body.onFloor()) { 
+						player.anims.play('right', true);
+					}else{
+						player.anims.play('rightJump', true);
+					}
+					lastPos = "right"
 				}
-				lastPos = "right"
-			}
-			if ((cursors.up.isDown || keyDown[2] == true ) && player.body.touching.down && !cursors.space.isDown  && keyDown[3] == false) { 
-			   player.setVelocityY(jumpPerso * -1)
-			   if(lastPos == "left"){
-					player.anims.play('turnLeft');
-				}
-				else{
-					player.anims.play('turnRight')
+				if ((cursors.up.isDown || keyDown[2] == true ) && player.body.onFloor() && !cursors.space.isDown  && keyDown[3] == false) { 
+				   player.setVelocityY(jumpPerso * -1)
+				   if(lastPos == "left"){
+						player.anims.play('turnLeft');
+					}
+					else{
+						player.anims.play('turnRight')
+					}
 				}
 			}
 		}
--- NEW PORTFOLIO/SCENES/sceneTiled.js
+++ NEW PORTFOLIO/SCENES/sceneTiled.js
@@ -1,68 +1,63 @@
-class scene2 extends Phaser.Scene{
+class sceneTiled extends Phaser.Scene{
 		
 		constructor(){
-			 super({key: 'scene2' });
+			 super({key: 'sceneTiled' });
 		}
 		preload () {
 			this.load.image('background', 'ASSETS/FIGHTGAME_Assets/ENVIRO/Background/background_new_color.png');
-			this.load.image('ground', 'ASSETS/FIGHTGAME_Assets/ENVIRO/LD/platform_full.png');
-			this.load.image('tree', 'ASSETS/FIGHTGAME_Assets/ENVIRO/LD/tronc4.png');
-			this.load.image('prop1', 'ASSETS/FIGHTGAME_Assets/ENVIRO/props/flower1.png');
-			this.load.image('prop2', 'ASSETS/FIGHTGAME_Assets/ENVIRO/props/liana1.png');
-			this.load.image('prop3', 'ASSETS/FIGHTGAME_Assets/ENVIRO/props/liana4.png');	
-			this.load.image('prop4', 'ASSETS/FIGHTGAME_Assets/ENVIRO/props/grass1.png');
 			this.load.image('checkPoint', 'ASSETS/FIGHTGAME_Assets/ITEMs/bonus1.png');
 			this.load.spritesheet('portal', 'ASSETS/FIGHTGAME_Assets/ITEMs/bonus4.png', { frameWidth: 25, frameHeight: 25});
-			this.load.image('chest', 'ASSETS/FIGHTGAME_Assets/ITEMs/chest.png');
-			this.load.image('texte1', 'ASSETS/TEXTE/texte1.png');
-			this.load.image('checkpointText', 'ASSETS/TEXTE/checkpoint.png');
 			this.load.spritesheet('dude', 'ASSETS/FIGHTGAME_Assets/CHARAs/NAMKA/running/running.png', { frameWidth: 19, frameHeight: 41 });
-			this.load.image('pnj1', 'ASSETS/FIGHTGAME_Assets/CHARAs/MCCREE/Idle.png');
 			this.load.spritesheet('dudeAttack', 'ASSETS/FIGHTGAME_Assets/CHARAs/NAMKA/attack/attack.png', { frameWidth: 71, frameHeight: 41 });
 			this.load.spritesheet('boom', 'ASSETS/FIGHTGAME_Assets/FXs/BOUM/BOOM_1.png', { frameWidth: 303, frameHeight: 79 });
+			this.load.image('tiles', 'ASSETS/adve/tiles.png');
+  			// Load the export Tiled JSON
+  			this.load.tilemapTiledJSON('map1', 'ASSETS/Map/map1.json');
 		}
 		create () {
 			listCheckPoint = [];
-			checkPoint = [100,450+vertical,null]
+			checkPoint = [100,250+vertical,null]
+			
 			
 			//ajout images Map
 			background = this.add.image(2000, window.innerHeight - (300*window.innerHeight/600), 'background').setScale(window.innerHeight/600);
-			platforms = this.physics.add.staticGroup();
-			tree = this.physics.add.staticGroup();
-			props = this.physics.add.staticGroup();
-			tree.create(50, 910+vertical, 'tree').setScale(5.5).refreshBody();
-			tree.create(711, 640+vertical, 'tree').setScale(3.5).refreshBody();
-			tree.create(1211, 790+vertical, 'tree').setScale(3.5).refreshBody();
-			tree.create(1411, 790+vertical, 'tree').setScale(3.5).refreshBody();
-			platforms.create(200, 568+vertical, 'ground').setScale(2.5).refreshBody();
-			platforms.create(700, 418+vertical, 'ground').setScale(2.5).refreshBody();
-			platforms.create(1300, 568+vertical, 'ground').setScale(2.5).refreshBody();
 
-
-
-			pnj1 = this.physics.add.sprite(830, 310+vertical, 'pnj1').setScale(3.75);
+			map = this.make.tilemap({ key: 'map1' });
+			const tileset = map.addTilesetImage('test', 'tiles');
+			backgroundTiled = map.createStaticLayer('BACKGROUND', tileset, 0, 0).setScale(scaleMap);
+			platformsTiled = map.createStaticLayer('COLLIDE', tileset, 0, 0).setScale(scaleMap);
+			deadTouchTiled = map.createStaticLayer('DEADTOUCH', tileset, 0, 0).setScale(scaleMap);
+			elementsTiled = map.createStaticLayer('ELEMENTS', tileset, 0, 0).setScale(scaleMap);
+			platformsTiled.setCollisionByExclusion(-1, true);
 			
-			checkPoint0 = this.physics.add.sprite(100, 510+vertical, 'checkPoint').setScale(2.5);
-			checkPoint0.body.allowGravity = false
-			listCheckPoint.push(checkPoint0)
-			portal = this.physics.add.sprite(1310, 510+vertical, 'portal').setScale(2.5);
-			portal.body.allowGravity = false
-
+			
+			function playerHit(player, deadTouchTiled) {
+			  //retourCheckPoint
+				boom.x = player.x
+				boom.y = player.y
+				player.x = checkPoint[0]
+				player.y = checkPoint[1]
+				boom.anims.play('boom', true);
+			}
+			
 			if(window.mobileCheck() == false){
-				boom = this.add.sprite(-10000, (window.innerHeight - (303*1.5/2)) , 'boom').setScale(1.5);
+				boom = this.add.sprite(-10000, (map.heightInPixels*scaleMap - (303*1.5/2)) , 'boom').setScale(1.5);
 			}else{
-				boom = this.add.sprite(-10000, (window.innerHeight - (303*1.5/2) - window.innerHeight*0.20) , 'boom').setScale(1.5);
+				boom = this.add.sprite(-10000, (map.heightInPixels*scaleMap- (303*1.5/2) - map.heightInPixels*scaleMap*0.20) , 'boom').setScale(1.5);
 			}
 			boom.rotation = 4.7
 
+			const spawnPoint = map.getObjectLayer('SPAWNPOINT')['objects'][0];
+			checkPoint = [spawnPoint.x*scaleMap,spawnPoint.y*scaleMap,null]
+			
 			player = this.physics.add.sprite(checkPoint[0],checkPoint[1], 'dude').setScale(3);
-			this.physics.add.collider(player, platforms);
-			this.physics.add.collider(pnj1, platforms);
+			this.physics.add.collider(player, platformsTiled);
 			cursors = this.input.keyboard.createCursorKeys()
 			var cam = this.cameras.main
-			cam.setBounds(0, 0, 3800, 600);
+			cam.setBounds(0, 0, map.widthInPixels*scaleMap, map.heightInPixels*scaleMap);
 			cam.startFollow(player,cam.FOLLOW_LOCKON, 0.1, 0.1);
 			var firstZoom = true
+			this.physics.add.collider(player, deadTouchTiled, playerHit, null, this);
 			function zoomCamera(){
 				if(firstZoom){
 					cam
@@ -85,35 +80,55 @@ class scene2 extends Phaser.Scene{
 			background.setScrollFactor(0.5);
 
 			//anim CHeckPoint
-			function checkPointF(checkPointSelectId){
-					checkPoint=[listCheckPoint[checkPointSelectId].x,listCheckPoint[checkPointSelectId].y-100,listCheckPoint[checkPointSelectId]]
+			function checkPointF(checkPointSelect){
+					checkPoint=[checkPointSelect.x,checkPointSelect.y-100,checkPointSelect]
 					if(startAnim == false){
 						startAnim = true;
 						var interval = setInterval(function (){
-							if(Math.round(listCheckPoint[checkPointSelectId].angle +180) < 360 && Math.round(listCheckPoint[checkPointSelectId].angle +180) > 0){
-								listCheckPoint[checkPointSelectId].angle += 3
+							if(Math.round(checkPointSelect.angle +180) < 360 && Math.round(checkPointSelect.angle +180) > 0){
+								checkPointSelect.angle += 3
 							}else{
 								startAnim = false
 								clearInterval(interval)
 							}
 						},1)
-						if(checkPointSelectId == 1){
-							var intervalN = setInterval(function (){
-								if(textTuto.alpha > 0){
-								   textTuto.alpha -= 0.1;
-								}else{
-									clearInterval(intervalN)
-								}
-							},100)
-						}
 					}
 
 				}
-
-			//listCheckPoint
-			this.physics.add.overlap(player, checkPoint0, function(){checkPointF(0)});
 			
-			 	
+			 	// Create a sprite group for all spikes, set common properties to ensure that
+			// sprites in the group don't move via gravity or by player collisions
+			checkPoints = this.physics.add.group({
+			  allowGravity: false,
+			  immovable: true
+			});
+			
+			// Let's get the spike objects, these are NOT sprites
+			const checkPointObjects = map.getObjectLayer('CHECKPOINT')['objects'];
+
+			// Now we create spikes in our sprite group for each object in our map
+			checkPointObjects.forEach(checkPointS => {
+			  // Add new spikes to our sprite group, change the start y position to meet the platform
+			  var myCheckPoint = this.physics.add.sprite(checkPointS.x*scaleMap, checkPointS.y*scaleMap  , 'checkPoint').setScale(2.5);
+			
+			  myCheckPoint.body.allowGravity = false;
+			  myCheckPoint['idS'] = listCheckPoint.length
+			  listCheckPoint.push(myCheckPoint)
+				
+				this.physics.add.overlap(player, myCheckPoint, function(){checkPointF(myCheckPoint)});
+			});
+			var portalObjects = []
+			const portalPointObjects = map.getObjectLayer('PORTAL')['objects'];
+
+			// Now we create spikes in our sprite group for each object in our map
+			portalPointObjects.forEach(checkPointS => {
+			  // Add new spikes to our sprite group, change the start y position to meet the platform
+			  var myPortalPoint = this.physics.add.sprite(checkPointS.x*scaleMap, checkPointS.y*scaleMap  , 'portal').setScale(2.5);
+			
+			  myPortalPoint.body.allowGravity = false;
+				portalObjects.push(myPortalPoint)
+			});
+			
 			//list animation
 			this.anims.create({
 				key: 'boom',
@@ -182,16 +197,22 @@ class scene2 extends Phaser.Scene{
 			sword = function (){
 				
 				player.setVelocityX(0);
+				
+				
+
+			// Now we create spikes in our sprite group for each object in our map
+			portalObjects.forEach(checkPointS => {
+				
 				//action si il est à coté du portal sinon juste attaque
 				if(lastPos == "left"){
-					if(portal.x - player.x > -100 && portal.x - player.x < 0){
+					if(checkPointS.x - player.x > -100 && checkPointS.x - player.x < 0){
 						zoomCamera()
 						setTimeout(function(){
 							player.anims.play('attackLeft', true);
-							portal.anims.play('portalAnim')
+							checkPointS.anims.play('portalAnim')
 							setTimeout(function(){
-								game.scene.start("selectWorld");
-								game.scene.stop("scene2");
+								game.scene.start("scene2");
+								game.scene.stop("sceneTiled");
 							},1000	)
 						},400)
 					}else{
@@ -199,21 +220,23 @@ class scene2 extends Phaser.Scene{
 					}
 				}
 				else{
-					if(portal.x - player.x < 100){
+					if(checkPointS.x - player.x < 100){
 						zoomCamera()
 						setTimeout(function(){
-							portal.anims.play('portalAnim')
+							checkPointS.anims.play('portalAnim')
 							player.anims.play('attackRight', true)
 							setTimeout(function(){
-								game.scene.start("selectWorld");
-								game.scene.stop("scene2");
+								game.scene.start("scene2");
+								game.scene.stop("sceneTiled");
 							},1000)
 						},400)
 					}else{
 						player.anims.play('attackRight', true)
 					}
 				}
+			});
 			}
+									   
 			
 			this.input.keyboard.on('keyup_SPACE', function (event) {
 				sword()
@@ -230,13 +253,6 @@ class scene2 extends Phaser.Scene{
 				sword()
 			}
 			
-			//animation pnj 
-			if(player.x < pnj1.x){
-				pnj1.flipX = true;
-			}else{
-				pnj1.flipX = false;
-			}
-			
 			
 			//animation checkpoint
 			for(var i = 0; i < listCheckPoint.length;i++){
@@ -245,14 +261,14 @@ class scene2 extends Phaser.Scene{
 				}
 			}
 
-			
 			//retourCheckPoint
-			if(player.y >  (window.innerHeight +100)){
+			if(player.y >  (map.heightInPixels*scaleMap +100)){
 				boom.x = player.x
 				player.x = checkPoint[0]
 				player.y = checkPoint[1]
 				boom.anims.play('boom', true);
 			}
+		
 			
 			//animation arret
 			if(cursors.left.isUp && cursors.right.isUp && keyDown[0] == false && keyDown[1] == false && player.body.velocity.x != 0){
@@ -265,26 +281,12 @@ class scene2 extends Phaser.Scene{
 				}
 			}
 			
-			//if plateforme dialogue avec pnj
-			if(player.x > 465 && player.x < 953 && player.y == 348 && !dialoguePnj1){
-				dialoguePnj1 = true
-				player.setVelocityX(0)
-				   if(lastPos == "left"){
-						player.anims.play('turnLeft');
-					}
-					else{
-						player.anims.play('turnRight')
-					}
-				dialogue("Hey, Hyacinthe ! <br> ca fait si longtemps ! ",5000)
-				setTimeout(function (){
-				},3000)
-			}
-			
+		
 			if(canMove == true){
 				//deplacement + saut
 				if (cursors.left.isDown || keyDown[1] == true) {
 					player.setVelocityX(speedPerso * -1);
-					if (player.body.touching.down) { 
+					if (player.body.onFloor()) { 
 						player.anims.play('left', true);
 					}else{
 						player.anims.play('leftJump', true);
@@ -293,14 +295,14 @@ class scene2 extends Phaser.Scene{
 				}
 				else if (cursors.right.isDown || keyDown[0] == true) {
 					player.setVelocityX(speedPerso);
-					if (player.body.touching.down) { 
+					if (player.body.onFloor()) { 
 						player.anims.play('right', true);
 					}else{
 						player.anims.play('rightJump', true);
 					}
 					lastPos = "right"
 				}
-				if ((cursors.up.isDown || keyDown[2] == true ) && player.body.touching.down && !cursors.space.isDown  && keyDown[3] == false) { 
+				if ((cursors.up.isDown || keyDown[2] == true ) && player.body.onFloor() && !cursors.space.isDown  && keyDown[3] == false) { 
 				   player.setVelocityY(jumpPerso * -1)
 				   if(lastPos == "left"){
 						player.anims.play('turnLeft');
