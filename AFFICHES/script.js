57ccbfdc2fcb11c90c50e59bce6a049412867e78
isaacmenard <menardisaac@gmail.com>
2020-12-26 13:29:47 +0100

    IMAGES COMPRESSSS

--- AFFICHES/script.js
+++ AFFICHES/script.js
@@ -2,11 +2,13 @@ var imgList = []
 closeImg()
 for(var i = 0; i< document.getElementsByClassName('card').length;i++){
   document.getElementsByClassName('card')[i].onclick = function(){
-    document.getElementById("imageToChange").src = this.getElementsByTagName("img")[0].src
+    arr = this.getElementsByTagName("img")[0].src.split("/")
+    index = arr.findIndex(el => el === "POSTERS_COMPRESS")
+    arr[index] = "POSTERS"
+    document.getElementById("imageToChange").src = arr.join("/")
     document.getElementById("ENGRAND").style.visibility = "visible"
   }
 }
-
 function closeImg(){
   document.getElementById("ENGRAND").style.visibility = "hidden"
 }
