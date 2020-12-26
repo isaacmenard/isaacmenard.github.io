e6443f9804e3a1357cc0d1186a328d9aebb07986
isaacmenard <menardisaac@gmail.com>
2020-12-26 13:56:12 +0100

    HELLLOOOOOOW finish kwa

--- AFFICHES/script.js
+++ AFFICHES/script.js
@@ -1,4 +1,5 @@
 var imgList = []
+var interval;
 closeImg()
 for(var i = 0; i< document.getElementsByClassName('card').length;i++){
   document.getElementsByClassName('card')[i].onclick = function(){
@@ -7,8 +8,17 @@ for(var i = 0; i< document.getElementsByClassName('card').length;i++){
     arr[index] = "POSTERS"
     document.getElementById("imageToChange").src = arr.join("/")
     document.getElementById("ENGRAND").style.visibility = "visible"
+    document.getElementById("parDessus").style.visibility = "visible"
+    interval = setInterval(function () {
+      if(document.getElementById("imageToChange").complete){
+        console.log("complete")
+        clearInterval(interval)
+            document.getElementById("parDessus").style.visibility = "hidden"
+      }
+    }, 10);
   }
 }
 function closeImg(){
+  clearInterval(interval)
   document.getElementById("ENGRAND").style.visibility = "hidden"
 }
