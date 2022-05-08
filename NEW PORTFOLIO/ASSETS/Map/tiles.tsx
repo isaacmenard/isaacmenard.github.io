423388823dce5ff6010290c96f41e95238fca792
Isaac MENARD <35576419+isaacmenard@users.noreply.github.com>
2022-05-08 15:12:39 +0200

    site updated

--- NEW PORTFOLIO/ASSETS/Map/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="test" tilewidth="8" tileheight="8" tilecount="504" columns="24">
- <image source="../adve/tiles.png" width="192" height="168"/>
-</tileset>
